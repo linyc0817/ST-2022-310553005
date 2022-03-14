7d8360a4d52f176c4e67f3da232b133a2c88c37a
linyc0817 <f0922571727@gmail.com>
2022-03-14 22:38:53 +0800

    Make fail build

--- src/test/java/PriorityQueueTest.java
+++ src/test/java/PriorityQueueTest.java
@@ -18,7 +18,8 @@ public class PriorityQueueTest {
     static Stream<Arguments> streamProvider() {
         return Stream.of(
                 arguments(new int[]{5, 4, 2, 3}, new int[]{2, 3, 4, 5}),
-                arguments(new int[]{-3,-1,-2,5}, new int[]{-3,-2,-1,5}),
+//                arguments(new int[]{-3,-1,-2,5}, new int[]{-3,-2,-1,5}),
+                arguments(new int[]{1,1,1,1}, new int[]{-3,-2,-1,5}),
                 arguments(new int[]{3,-2,-5,-1,2}, new int[]{-5,-2,-1,2,3}),
                 arguments(new int[]{-3,1,11,0,9,3}, new int[]{-3,0,1,3,9,11}),
                 arguments(new int[]{3,7,2,-1,-2}, new int[]{-2,-1,2,3,7})
